903dec52b92c02ea00db6ce18d70631b1d06cf6e
Jens Mischer <jens.mischer@googlemail.com>
2018-12-09 20:02:26 +0100

    Use ModuleHelper

--- src/mod_content_image/mod_content_image.php
+++ src/mod_content_image/mod_content_image.php
@@ -1,16 +1,24 @@
 <?php
+use Joomla\CMS\Helper\ModuleHelper;
+
 /**
  * 
  * @var array $params
  */
-
 // No direct access
 defined('_JEXEC') or die;
 
 // Include the syndicate functions only once
 require_once dirname(__FILE__) . '/helper.php';
 
+// Get mod content image helper data
 $data = ModContentImageHelper::getData($params);
 
-require JModuleHelper::getLayoutPath('mod_content_image');
+// Return, if data empty
+if (!$data) {
+	return false;
+}
+
+// Get layout path and render template
+require ModuleHelper::getLayoutPath('mod_content_image');
 ?>
